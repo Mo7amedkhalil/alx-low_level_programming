cc9c83385b4c3fa21135e8f81d73aaea7a3bf0ca
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-02-16 06:51:09 -0800

    done !

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 
 /**
-  * main - a program that prints a specified mssg
-  *Return: 1 (success)
+  * main - a program that prints a specified msg
+  * Return: 0 (sucess)
   */
 int main(void)
 {
-	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
 	return (1);
 }
-
