4c5a25b7fde7c1fb0258cc0f876bab5472ab2e7c
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-02-19 03:29:32 -0800

    done !

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - program prints all possible combinations of single-digit numbers
+  *
+  * Return: 0 (success)
+  */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+		putchar(',');
+	}
+	return (0);
+}
