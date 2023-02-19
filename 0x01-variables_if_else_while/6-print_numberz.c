e19e92dd8f3267145623e94a462647d7ed800fba
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-02-19 02:42:26 -0800

    done !

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,13 +8,10 @@
 
 int main(void)
 {
-	int x[10] = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
 	int i;
 
 	for (i = 0; i < 10; i++)
-	{
-		putchar(x[i]);
-	}
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
