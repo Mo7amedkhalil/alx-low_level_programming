2e2c7c6db4ab4b7aed54e7340ed2a21257e4c6af
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-02-19 04:08:07 -0800

    done !

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int i = 0;
 
-	while (i < 10 )
+	while (i < 10)
 	{
 		putchar(48 + i);
 		if (i != 9)
 		{
 			putchar(',');
-			putchar(' ');
 		}
+		putchar(' ');
 		i++;
 	}
 	return (0);
