56e3a3b02415141fe9e79d9dc70c8698892ea318
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-02-18 22:38:42 -0800

    done !

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main -  a program that prints the alphabet in lowercase, followed by a new line.
+  *
+  * Return: 0 (success)
+  */
+int main (void)
+{
+	 char A;
+
+	    for(A = 'a' ; A <= 'z' ; A++)
+	    {
+		          putchar(A);
+	    }
+	    return 0;
+}
