c24c03c3bf6b7621f7c3f70245373f125f39c49c
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-02-19 03:20:25 -0800

    dd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main -  a program that prints Hexadecimal numbers in lowercase
+  *
+  * Return: 0 (success)
+  */
+
+int main (void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+		putchar(i % 10 + '0');
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
