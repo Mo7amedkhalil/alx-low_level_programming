8004a141a60d579ed2965cbd7a70da963754d365
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-02-22 07:26:25 -0800

    done !

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * print_to_98 - prints from i/p num to 9
+  * @n: input num
+  */
+
+void print_to_98(int n)
+{
+	if (n < 98 )
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
+		
+		}
+	}
+	else
+	{
+		for (i =n; i >= 98; i--)
+		{
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
+		}
+	}
+}
