554db7740cb2e955323492483a289b80f1b46935
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-02-22 06:06:09 -0800

    done !

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 			{
 				_putchar(s + '0');
 			}
-			else if(s < 10 && s != 0)
+			else if (s < 10 && s != 0)
 			{
 				_putchar(s + '0');
 			}
@@ -37,8 +37,9 @@ void times_table(void)
 				_putchar((s / 10) + '0');
 				_putchar((s % 10) + '0');
 			}
+			}
 		}
-		_putchar('\n');
-	}
+			
+	_putchar('\n');
 	}
 }
