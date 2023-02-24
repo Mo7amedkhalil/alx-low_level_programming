e3aad0aaee51c49b5b1bef33bb5917008870b0af
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-02-24 08:37:20 -0800

    done !

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,7 @@
 
 /**
   * main - program to test a function
+  * if positive or negative
   *
   * Return: 0
   */
@@ -10,6 +11,6 @@ int main(void)
 {
 	int i = 0;
 
-	postitive_or_negative(i);
+	positive_or_negative(i);
 	return (0);
 }
