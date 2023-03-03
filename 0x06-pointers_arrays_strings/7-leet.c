c764c586c56e525be245d722c579b13142dd9652
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-03-02 17:02:59 -0800

    done !

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * leet - a function that encodes a string into 133
+  * @x: input string
+  *
+  * Return: edited string
+  */
+
+char *leet(char *x)
+{
+	int i, j;
+	char y1[] = "aAeEoOtTlL";
+	char y2[] = "4433007711";
+
+	for (i = 0; x[i] < '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (x[i] == y1[j])
+				x[i] = y2[j];
+		}
+	}
+	return (x);
+}
