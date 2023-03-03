f3c92978e43dd24d81af440aabe4f6f0d50a2a77
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-03-02 17:14:12 -0800

    done !

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,17 +11,20 @@ char *cap_string(char *str)
 	int c = 32;
 	int s[] = {',', ';', '.', '?', '"',
 		 '(', ')', '{', '}', ' ', '\n', '\t'};
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - c;
 		c = 0;
 		for (j = 0; s[j] <= 12; j++)
+		{
 			if (str[i] == s[j])
 			{
 				j = 12;
 				c = 32;
 			}
+		}
 	}
 	return (str);
 }
