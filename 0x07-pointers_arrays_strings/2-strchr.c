0d255a9b2836ed6a876c2b2054b1058b2f6b49ee
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-03-07 12:21:57 -0800

    done !

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: pointed string
+ * @c: char to be found
+ * Return: Always 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] < '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (NULL);
+}
