de90753b57cf5b02f3ae705a2ec2895b96139acb
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-03-08 17:45:10 -0800

    done !

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,31 @@
 #include "main.h"
 
+int theprime(int x, int y);
 /**
   * is_prime_number - checks if prime number
   * @n: input num
   * Return: 1 if prime, 0 otherwise
   */
 
+int is_prime_number(int n)
+{
+	if (n <=1)
+		return (0);
+	return (theprime(n, n - 1));
+}
+
+/**
+  * the prime - checks if prime
+  * @x: the num
+  * @y: iterator
+  * Return: the prime num
+  */
+
+int theprime(int x, int y)
+{
+	if (y == 1)
+		return (1);
+	if (x % y == 0 && y > 0)
+		return (0);
+	return (theprime(x, y - 1));
+}
