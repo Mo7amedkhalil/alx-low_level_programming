709bc5b1c89c4f228e2dfd3bcabea4d9dbc0b599
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-03-14 16:55:48 -0700

    done !

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of the array
+ * @c: char to initialize the array with
+ * Return: pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr = malloc(size * sizeof(char));
+
+	if (size == 0 || ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		*(ptr + i) = c;
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - creates an array of chars
+ * @s1: string
+ * @s2: string
+ * Return: pointer to the new string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int s1l = 0;
+	int s2l = 0;
+	int i;
+	int j;
+	char *newstr;
+
+	if (s1 == NULL)
+		s1l = 0;
+	else
+	{
+		while (s1[s1l] != '\0')
+			s1l++;
+	}
+
+	if (s2 == NULL)
+		s2l = 0;
+	else
+	{
+		while (s2[s2l] != '\0')
+		s2l++;
+	}
+
+	newstr = malloc(sizeof(char) * (s1l + s2l + 1));
+	return (newstr);
+}
