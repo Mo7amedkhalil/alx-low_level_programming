c7c06926a714ef693b1988a716690ae10159dfb4
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-03-21 19:25:20 -0700

    done !

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,23 +3,18 @@
 #include "dog.h"
 
 /**
- * init_dog - initializes the empty fields of the struct dog
- * @d: pointer to the struct
- * @name: name string
- * @age: the age of the dog
- * @owner: owner name string
- * Return: Success
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+  * print_dog - function that prints struct dog
+  * @d: struct dog to print
+  */
+
+void print_dog(struct dog  *d)
 {
 	if (d == NULL)
-	{
-		d = malloc(sizeof(struct dog));
-		if (d == NULL)
-			return;
-	}
-
-	d->age = age;
-	d->name = name;
-	d->owner = owner;
+		return;
+	if (d->name == NULL)
+		(*d).name = "(nil)";
+	if (d->owner == NULL)
+		(*d).owner = "(nil)";
+	
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
