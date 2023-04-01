da6ef5002715a75a48016ef23b3bb56ea3e73257
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-03-31 19:05:41 -0700

    done !

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,13 @@ size_t print_list(const list_t *h)
 {
 	size_t c = 0;
 
-	while {h}
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		h - h->next;
+		h = h->next;
 		c++;
 	}
 	return (c);
