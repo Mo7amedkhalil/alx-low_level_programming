a71d4911c895c51ba9f1b4e6aff0d307f45d8018
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-03-31 19:20:04 -0700

    done !

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * list_len - function that returns the number of elements in a linked list_t lis
+  * @h: ponter to list
+  *
+  * Return: no of elements
+  */
+
+size_t list_len(const list_t *h)
+{
+	size_t c = 0;
+
+	while (h)
+	{
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
