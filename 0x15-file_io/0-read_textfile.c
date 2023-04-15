b8d16b2693893fabe69d068aa1021ee206f87ebb
Mo7amedkhalil <mo7amed.khalil018@gmail.com>
2023-04-14 18:23:27 -0700

    done !

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,1 +1,38 @@
-ghb
+#include "main.h"
+
+/**
+ * read_textfile - reads a file and prints its content to standard output
+ * @filename: string
+ * @letters: number of letters to be printed
+ * Return: number of letters printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, n;
+	char *x;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	x = malloc(sizeof(char) * letters);
+	if (!x)
+		return (0);
+	n = read(fd, x, letters);
+	if (n == -1)
+	{
+		free(x);
+		return (0);
+	}
+	n = write(STDOUT_FILENO, x, n);
+	if (n == -1)
+	{
+		free(x);
+		return (0);
+	}
+	close(fd);
+	free(x);
+	return (n);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,1 +1,15 @@
-bgj
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _strlen(char *text);
+void myExit(char *message, char *filename, int code, int fd);
+
+#endif
